c620fc495d014212405e77b3c92179866aae29ff
Alexey Osipenko <alexey.osipenko@gmail.com>
2015-06-07 20:17:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.log("Request for host " + request.headers.host);
-  response.send('Hello World!')
+  response.send('Hello World skdfbvsdf!')
 })
 
 app.listen(app.get('port'), function() {
