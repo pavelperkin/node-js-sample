6d306ca776f16aaddf502886e655e9d74bcb386d
Alexey Osipenko <alexey.osipenko@gmail.com>
2015-11-19 20:32:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.log(new Date + "Request for host " + request.headers.host);
-  response.send('Hello Startup Crush Test!')
+  response.send('Hello example!')
 })
 
 app.listen(app.get('port'), function() {
