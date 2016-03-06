6369a24323599014040f517dba222a29522ddac0
pavelperkin <githubpp@gmail.com>
2016-03-06 20:16:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   console.log("Request for host " + request.headers.host);
-  response.send('Hello some another branch!=)')
+  response.send('Hello some another branch!=) How are you?')
 })
 
 app.listen(app.get('port'), function() {
